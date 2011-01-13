62325ad818c00614b41cbb7936552c41412c62bb
Sergey Kornitsky <kornitsky@ukr.net>
2011-01-14 00:58:45 +0200

    initial release

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+  include Facebooker2::Rails::Controller
+
+  protect_from_forgery
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbcomments::Application.config.secret_token = 'a10d95649793feab5b37b15d81396efc3a5e69ff8322c88085eefd670f20b434cd9f329c31f1e9f24620ccd8b4451c2f55007d7af8a55c9bc4c933a41d2639c5'
